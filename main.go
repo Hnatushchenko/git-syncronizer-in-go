4de4c4550d7b5d34bc55c75a89935c48a3a0da62
Andrii Hnatushchenko <andrii.hnatushchenko@gmail.com>
2024-03-11 08:05:41 +0200

    coping successfully

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-    fileHelper "github.com/hnatushchenko/git-syncronizer/helpers"
-    "log"
+	fileHelper "github.com/hnatushchenko/git-syncronizer/helpers"
+	"log"
 )
 
-const sourcePath = "C:\\Users\\gnatu\\mydisk\\L35\\repos\\test2\\luminaryone"
-const destinationPath = "C:\\Users\\gnatu\\mydisk\\L35\\src2\\test"
+const sourcePath = "C:\\test4\\screens"
+const destinationPath = "C:\\test4\\destination"
 
 func main() {
-    validateInputDirectories()
-    removeFilesRecursively(destinationPath)
-    copyFilesRecursively(sourcePath, sourcePath, destinationPath, GetFileNamesToIgnore(), GetDirectoryNamesToIgnore())
+	validateInputDirectories()
+	copyFilesUsingGoRoutines(sourcePath, destinationPath)
 }
 
 func validateInputDirectories() {
-    exists, err := fileHelper.FileExists(sourcePath)
-    if err != nil {
-        log.Println("Failed to check the existence of the source directory.")
-        log.Fatalln(err)
-    }
-    if !exists {
-        log.Fatalln("The source directory does not exist!")
-    }
-    
-    exists, err = fileHelper.FileExists(destinationPath)
-    if err != nil {
-        log.Println("Failed to check the existence of the destination directory.")
-        log.Fatalln(err)
-    }
-    if !exists {
-        log.Fatalln("The destination directory does not exist!")
-    }
+	exists, err := fileHelper.FileExists(sourcePath)
+	if err != nil {
+		log.Println("Failed to check the existence of the source directory.")
+		log.Fatalln(err)
+	}
+	if !exists {
+		log.Fatalln("The source directory does not exist!")
+	}
+
+	exists, err = fileHelper.FileExists(destinationPath)
+	if err != nil {
+		log.Println("Failed to check the existence of the destination directory.")
+		log.Fatalln(err)
+	}
+	if !exists {
+		log.Fatalln("The destination directory does not exist!")
+	}
 }
