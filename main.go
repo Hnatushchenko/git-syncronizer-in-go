a51819c800b6438fe5e051b7918baa97750b619f
Andrii Hnatushchenko <andrii.hnatushchenko@gmail.com>
2024-03-14 09:52:35 +0200

    save main.go

--- main.go
+++ main.go
@@ -1,43 +1,35 @@
 package main
 
 import (
-	"fmt"
-	fileHelper "github.com/hnatushchenko/git-syncronizer/helpers"
-	"io"
 	"log"
-	"time"
+	"os/exec"
 )
 
 const sourcePath = "C:\\test4\\screens"
 const destinationPath = "C:\\test4\\destination"
 
 func main() {
-	//validateInputDirectories()
-	log.SetOutput(io.Discard)
-	removeFilesRecursively(destinationPath)
-	start := time.Now()
-	numOfBytes := copyFilesUsingGoRoutines(sourcePath, destinationPath)
-	elapsed := time.Since(start)
-	fmt.Printf("Coping took %s\r\n", elapsed)
-	fmt.Printf("%v bytes copied", numOfBytes)
+	executeChangeDirectory()
+	app := "git"
+	arg0 := "status"
+	cmd := exec.Command(app, arg0)
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(stdout))
 }
 
-func validateInputDirectories() {
-	exists, err := fileHelper.FileExists(sourcePath)
+func executeChangeDirectory() {
+	app := "cd"
+	arg0 := "C:\\Users\\gnatu\\source\\repos\\L35\\github\\luminaryone-github-1"
+	cmd := exec.Command(app, arg0)
+	stdout, err := cmd.Output()
 	if err != nil {
-		log.Println("Failed to check the existence of the source directory.")
-		log.Fatalln(err)
-	}
-	if !exists {
-		log.Fatalln("The source directory does not exist!")
+		log.Fatal(err)
 	}
 
-	exists, err = fileHelper.FileExists(destinationPath)
-	if err != nil {
-		log.Println("Failed to check the existence of the destination directory.")
-		log.Fatalln(err)
-	}
-	if !exists {
-		log.Fatalln("The destination directory does not exist!")
-	}
+	log.Println(string(stdout))
+	log.Println("Changed directory")
 }
